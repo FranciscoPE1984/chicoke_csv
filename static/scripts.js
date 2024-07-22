d386faeb49293ad2e7f85acf1283c9bd77b67946
FranciscoPE1984 <123676214+FranciscoPE1984@users.noreply.github.com>
2024-07-22 15:32:43 -0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -6,9 +6,22 @@ document.getElementById('searchForm').addEventListener('submit', function (event
         .then(response => response.json())
         .then(data => {
             const resultsDiv = document.getElementById('results');
-            resultsDiv.innerHTML = '<ul>' +
-                data.map(item => `<li><strong>${item.codigo}</strong>: ${item.interprete} - ${item.titulo}</li>`).join('') +
-                '</ul>';
+            resultsDiv.innerHTML = ''; // Limpar o conteúdo existente
+
+            if (data.message) {
+                const errorMessageDiv = document.createElement('div');
+                errorMessageDiv.className = 'error-message';
+                errorMessageDiv.innerHTML = `
+                    <p>Nenhum cantor ou música encontrado!</p>
+                    <p>Tente novamente como parte do nome do cantor ou música!</p>
+                    <p>Atente para o nome correto do cantor ou música!</p>
+                `;
+                resultsDiv.appendChild(errorMessageDiv);
+            } else {
+                resultsDiv.innerHTML = '<ul>' +
+                    data.map(item => `<li><strong>${item.codigo}</strong>: ${item.interprete} - ${item.titulo}</li>`).join('') +
+                    '</ul>';
+            }
         })
         .catch(error => {
             console.error('Erro ao buscar dados:', error);
