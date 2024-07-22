50081c963f3454534cef8a1360b27be17a6ddf67
FranciscoPE1984 <123676214+FranciscoPE1984@users.noreply.github.com>
2024-07-22 06:11:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, send_from_directory
+import pandas as pd
+
+app = Flask(__name__)
+
+# Carregar dados do CSV
+csv_file_path = 'musicas.csv'
+df = pd.read_csv(csv_file_path, delimiter=';', encoding='latin1')
+
+@app.route('/')
+def index():
+    return send_from_directory('templates', 'index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'Query parameter is required'}), 400
+
+    # Filtrar o DataFrame com base na consulta
+    results = df[df.apply(lambda row: query.lower() in row['titulo'].lower() or query.lower() in row['interprete'].lower(), axis=1)]
+
+    # Converter o DataFrame filtrado para uma lista de dicionários
+    results = results.to_dict(orient='records')
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
